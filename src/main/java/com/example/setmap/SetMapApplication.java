7533f8e1995d7035e51ed5e9dc0556c2c801b829
Лаисат <lz_256@mail.ru>
2024-04-18 12:38:43 +0300

    SetM

---
+++ src/main/java/com/example/setmap/SetMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.setmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/setmap/SetMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
